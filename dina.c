058f02986e7a5e33f0ef5deb03a2ba39a167cfaa
root <root@kali>
2020-04-04 18:09:40 +0400

    demande multiplayer et demande dificultE du jeux

--- dina.c
+++ dina.c
@@ -1,36 +1,144 @@
 #include <stdio.h>
-#include <stdlib.h>
-#define MAX 100
-#define MIN 1
+#include <math.h>
+#include <time.h>
+#include "stdlib.h"
  
-char plus_moin()
+int partie_facile ( )
 {
-    int valeur_a_trouver,choix_joueur = 0;
-    valeur_a_trouver = (rand() % (MAX - MIN + 1)) + MIN;
-    char result;
- 
-    printf("Entrer un nombre: \n");
-    for (int i=0;i<5;i++)
+    int choix, note;
+    int hazar=rand() % 25+1;
+    int x;
+    note=0;
+    printf("Mode du jeux Facile: \n");
+    printf("Devigne un Nombre compris entre 1 et 25: \n");
+    for( x=1; x<=5; x++)
     {
-        scanf("%d", &choix_joueur);
- 
-        if (choix_joueur == valeur_a_trouver)
-            result=="Bingo";
- 
-        else {
-            if (choix_joueur > valeur_a_trouver)
-            result=="Moins";
-         
-            else result=="Plus";
-             } 
+        printf("Devignez: ");
+        scanf("%d",&choix);
+        if(choix==hazar)
+        {
+            printf("Bingo !!\n");
+            x=6;
+            note=note+1;
+        }
+        else if(choix>hazar)
+        {
+            printf("Le Nombre est MOINS !\n");
+            printf("Il te reste %d tentative\n",5-x);
+        }
+        else if(choix<hazar)
+        {
+            printf("Le Nombre est PLUS !\n");
+            printf("Il te reste %d tentative\n",5-x);
+        }
+        if(x==5 && choix!=hazar)
+        {
+            printf("Perdu ! Le Nombre etait %d ",hazar);
+        }
     }
-    return result;
+    return note;
 }
-
+int partie_moyen ( )
+{
+    int choix, note;
+    int hazar=rand() % 50+1;
+    int x;
+    note=0;
+    printf("Mode du jeux Moyen: \n");
+    printf("Devigne un Nombre compris entre 1 et 50: \n");
+    for( x=1; x<=5; x++)
+    {
+        printf("Devignez: ");
+        scanf("%d",&choix);
+        if(choix==hazar)
+        {
+            printf("Bingo !!\n");
+            x=6;
+            note=note+1;
+        }
+        else if(choix>hazar)
+        {
+            printf("Le Nombre est MOINS !\n");
+            printf("Il te reste %d tentative\n",5-x);
+        }
+        else if(choix<hazar)
+        {
+            printf("Le Nombre est PLUS !\n");
+            printf("Il te reste %d tentative\n",5-x);
+        }
+        if(x==5 && choix!=hazar)
+        {
+            printf("Perdu ! Le Nombre etait %d ",hazar);
+        }
+    }
+    return note;
+}
+int partie_difficile ( )
+{
+    int choix, note;
+    int hazar=rand() % 100+1;
+    int x;
+    note=0;
+    printf("Mode du jeux Difficile: \n");
+    printf("Devigne un Nombre compris entre 1 et 100: \n");
+    for( x=1; x<=5; x++)
+    {
+        printf("Devignez: ");
+        scanf("%d",&choix);
+        if(choix==hazar)
+        {
+            printf("Bingo !!\n");
+            x=6;
+            note=note+1;
+        }
+        else if(choix>hazar)
+        {
+            printf("Le Nombre est MOINS !\n");
+            printf("Il te reste %d tentative\n",5-x);
+        }
+        else if(choix<hazar)
+        {
+            printf("Le Nombre est PLUS !\n");
+            printf("Il te reste %d tentative\n",5-x);
+        }
+        if(x==5 && choix!=hazar)
+        {
+            printf("Perdu ! Le Nombre etait %d ",hazar);
+        }
+    }
+    return note;
+}
+ 
 int main()
 {
-    int choix=0;
-    printf("Ceci est un jeux de devinette,choisi un chiffre entre 1 et 100\n");
-    scanf("%lf",choix);
-    printf(plus_moin(choix));
+    int note,y;
+    int partie;
+    srand ( time(NULL) );
+    note=0;
+    printf("Choisir mode du jeux: \n");
+    printf("1- Mode Facile : 1 - 25\n");
+    printf("2- Mode Moyen : 1 - 50\n");
+    printf("3- Mode Difficile : 1 - 100\n");
+    scanf("%d",&partie);
+    for(y=1; y<=10; y++)
+    {
+        switch(partie)
+        {
+            case 1 :
+                note+=partie_facile();
+                printf("  Ta Note pour l' instant est de %d / %d\n",note,y);
+                break;
+            case 2 :
+                note+=partie_moyen();
+                printf("  Ta Note pour l' instant est de %d / %d\n",note,y);
+                break;
+            case 3 :
+                note+=partie_difficile();
+                printf("  Ta Note pour l' instant est de %d / %d\n",note,y);
+                break;
+            default :
+                printf("Ce choix n'est pas valable\n");
+        }
+        
+    }
 }
--- dina.sh
+++ dina.sh
@@ -2,9 +2,11 @@
 
 echo "Bonjour,ceci est un jeux de devinette"
 
-PS3="choisir le mode de jeux: "
-select mod_jeu in facile moyen difficile
+PS3="Nombre de joueur: "
+select joueur in un_player multi_player
 do
-echo “Vous avez choisi le mode $mod_jeu”
+echo “Vous avez choisi le mode $joueur”
+if (( joueur=1 ))
+	
 break
 done
