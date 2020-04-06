4a918010a575edb74e12bd79bafa03057859f6a4
root <root@kali>
2020-04-06 16:14:08 +0400

    modification pour deux joueur

--- dina.sh
+++ dina.sh
@@ -2,11 +2,15 @@
 
 echo "Bonjour,ceci est un jeux de devinette"
 
-PS3="Nombre de joueur: "
-select joueur in un_player multi_player
+PS3="Choisir nombre de joueur: "
+select joueur in un_joueur multi_joueur
 do
-echo “Vous avez choisi le mode $joueur”
-if (( joueur=1 ))
-	
-break
+	echo “Vous etes sur le mode $joueur”
+	break
 done
+if [[ $joueur == un_joueur ]]
+then
+	./un_player
+else
+	./multi_player
+fi
